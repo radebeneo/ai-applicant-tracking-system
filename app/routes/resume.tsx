3ca5bd489e7e1681e74d82f9381fd0e0dd2b9863
radebeneo <radebeneo17@gmail.com>
2025-09-14 13:24:27 +0200

    rendered Summary, ATS and Details components in resume review page.

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -1,6 +1,9 @@
 import {Link, useNavigate, useParams} from "react-router";
 import {useEffect, useState} from "react";
 import {usePuterStore} from "~/lib/puter";
+import Summary from "~/components/Summary";
+import ATS from "~/components/ATS";
+import Details from "~/components/Details";
 
 
 export const meta =() =>([
@@ -14,7 +17,7 @@ const Resume = () => {
     const { id } = useParams();
     const [imageUrl, setImageUrl] = useState('');
     const [resumeUrl, setResumeUrl] = useState('');
-    const [feedback, setFeedback] = useState('');
+    const [feedback, setFeedback] = useState<Feedback | null >(null  );
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -74,7 +77,10 @@ const Resume = () => {
                     <h2 className="text-4xl !text-black font-bold">Resume Review</h2>
                     {feedback ? (
                         <div className="flex flex-col gap-8 animate-in fade-in duration-1000">
-                            Summary ATS Details
+                            <Summary feedback = {feedback}/>
+                            <ATS score = {feedback.ATS.score || 0} suggestions = {feedback.ATS.tips || []}/>
+                            <Details feedback={feedback}/>
+
                         </div>
                     ) : (
                         <img src ="/images/resume-scan-2.gif"  className="w-full"/>
